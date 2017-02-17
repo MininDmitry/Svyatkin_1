338c767f1e6b973e06dd4ac44e660da97dd91280
MininDmitry <ya-minin.dmitry@yandex.ru>
2017-02-17 11:33:40 +0200

    Initial commit

---
+++ src/_1/Main.java
@@ -0,0 +1,16 @@
+package _1;
+
+/**
+ * Created by Dmitriy on 17.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("hi");
+        Object o = new Object();
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) System.out.println(" i  = " + i);
+
+        }
+    }
+}
